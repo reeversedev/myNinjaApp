cccc71025a6d9c934d327d99649ca8ad73dede0b
Prateek Gogia <prateek.gogia@semusi.com>
2017-11-13 16:58:47 +0530

    Done till json and http

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
-var myNinjaApp = angular.module('myNinjaApp', []);
+var myNinjaApp = angular.module('myNinjaApp', ['ngRoute']);
 
-myNinjaApp.controller('NinjaController', ['$scope', function ($scope) {
+myNinjaApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'views/home.html'
+        })
+        .when('/directory', {
+            templateUrl: 'views/directory.html',
+            controller: 'NinjaController'
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
+}]);
+
+myNinjaApp.controller('NinjaController', ['$scope', '$http', function ($scope, $http) {
 
     $scope.message = "hey y'll!";
-    $scope.ninjas = [
-        {
-            name: 'Yoshi',
-            belt: 'green',
-            rate: 50,
+    $scope.removeNinja = function (ninja) {
+        var removedNinja = $scope.ninjas.indexOf(ninja);
+        $scope.ninja.splice(removedNinja, 1);
+    }
+    $scope.addNinja = function () {
+        $scope.ninjas.push({
+            name: $scope.newninja.name,
+            belt: $scope.newninja.belt,
+            rate: parseInt($scope.newninja.rate),
             available: true
-        },
-        {
-            name: 'Crystal',
-            belt: 'Yellow',
-            rate: 30,
-            available: true
-        },
-        {
-            name: 'ryu',
-            belt: 'orange',
-            rate: 10,
-            available: false
-        },
-        {
-            name: 'Shaun',
-            belt: 'black',
-            rate: 1000,
-            available: true
-        }
-    ];
+        });
+        $scope.newninja.name = "";
+        $scope.newninja.belt = "";
+        $scope.newninja.rate = "";
+    };
+
+    $http.get('data/ninjas.json').then(function(response) {
+        $scope.ninjas = response.data;
+    })
 
 }]);
